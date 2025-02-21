e1780576da77b8d6663ba3e84dca5df08466b8a5
mayumon <malu.brisighello@mail.utoronto.ca>
2025-02-20 19:20:00 -0500

    added basic blog script (pick and show)

---
+++ blog.js
@@ -0,0 +1,28 @@
+// todo: add posts fetching
+const posts = ["example_post.md"];
+
+const postList = document.getElementById("post-list");
+const postContent = document.getElementById("post-content");
+
+function loadPost(post){
+    fetch(`blog_posts/${post}`)
+        .then(response => response.text())
+        .then(markdown => {postContent.innerHTML = marked.parse(markdown)})
+        .catch(error => console.error("failed to load post:", error));
+}
+
+// display posts
+posts.forEach(post => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+
+    // format post title (currently filename)
+    link.textContent = post.replace(".md", "")
+        .replace("_", " ");
+
+    link.onclick = () => loadPost(post)
+
+    li.appendChild(link);
+    postList.appendChild(li);
+})
---
+++ blog_posts/example_post.md
@@ -0,0 +1,18 @@
+# too slow
+## when the moon
+### is with you
+
+-  i'll be too 
+- and **wish**
+
+_you_ ~~up~~
+
+> with me
+
+---
+
+to come
+
+[soon](https://www.nitrome.com/html5-games/badicecream3/ "o.o")
+
+![:p](https://avatars.githubusercontent.com/u/113377248?v=4&size=64)
