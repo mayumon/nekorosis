038c9037ad65c77d42976a5037389638cb3da71d
mayumon <malu.brisighello@mail.utoronto.ca>
2025-07-10 09:23:54 -0400

    added letter picker for accent 1, accent 2, emotion, and accessory style

--- js/header.js
+++ js/header.js
@@ -50,58 +50,82 @@ function showCustomizePopup() {
     const overlay = document.createElement('div');
     overlay.className = 'popup-overlay';
 
+    // colour picker setup
+    const colours = [
+        '#e7dee3',
+        '#70686c',
+        '#373335',
+        '#f1d9bb',
+        '#e7a457',
+        '#5e3e35',
+        '#bce04c'
+    ];
+
+    const swatchesHTML = colours.map(c =>
+        `<div class="colour-option" data-colour="${c}"></div>`
+    ).join('');
+
+    // letter picker setup
+    const letters = ['none','a','b','c','d','e','f','g'];
+
     overlay.innerHTML = `
     <div class="popup-box">
       <button class="close">&times;</button>
       <div class="popup-content">
+
         <div class="popup-left">
 
           <div class="color-group" data-group="main">
             <h4>Main</h4>
-            <div class="colour-options">
-              <div class="colour-option" data-colour="#e7dee3"></div>
-              <div class="colour-option" data-colour="#70686c"></div>
-              <div class="colour-option" data-colour="#373335"></div>
-              <div class="colour-option" data-colour="#f1d9bb"></div>
-              <div class="colour-option" data-colour="#e7a457"></div>
-              <div class="colour-option" data-colour="#5e3e35"></div>
-              <div class="colour-option" data-colour="#bce04c"></div>
-            </div>
+            <div class="colour-options">${swatchesHTML}</div>
           </div>
 
           <div class="color-group" data-group="accent1">
             <h4>Accent 1</h4>
-            <div class="colour-options">
-              <div class="colour-option" data-colour="#e7dee3"></div>
-              <div class="colour-option" data-colour="#70686c"></div>
-              <div class="colour-option" data-colour="#373335"></div>
-              <div class="colour-option" data-colour="#f1d9bb"></div>
-              <div class="colour-option" data-colour="#e7a457"></div>
-              <div class="colour-option" data-colour="#5e3e35"></div>
-              <div class="colour-option" data-colour="#bce04c"></div>
+            
+            <div class="letter-group" data-group="accent1">
+              <button class="arrow left">&lt;</button>
+              <span class="letter">none</span>
+              <button class="arrow right">&gt;</button>
             </div>
+            
+            <div class="colour-options">${swatchesHTML}</div>
+            
           </div>
 
           <div class="color-group" data-group="accent2">
             <h4>Accent 2</h4>
-            <div class="colour-options">
-              <div class="colour-option" data-colour="#e7dee3"></div>
-              <div class="colour-option" data-colour="#70686c"></div>
-              <div class="colour-option" data-colour="#373335"></div>
-              <div class="colour-option" data-colour="#f1d9bb"></div>
-              <div class="colour-option" data-colour="#e7a457"></div>
-              <div class="colour-option" data-colour="#5e3e35"></div>
-              <div class="colour-option" data-colour="#bce04c"></div>
+            
+            <div class="letter-group" data-group="accent2">
+              <button class="arrow left">&lt;</button>
+              <span class="letter">none</span>
+              <button class="arrow right">&gt;</button>
             </div>
+            
+            <div class="colour-options">${swatchesHTML}</div>
+            
           </div>
 
-          <h4>Emotion</h4>
-          <h4>Accessory</h4>
+          <div class="letter-group" data-group="emotion">
+            <h4>Emotion</h4>
+            <button class="arrow left">&lt;</button>
+            <span class="letter">none</span>
+            <button class="arrow right">&gt;</button>
+          </div>
+
+          <div class="letter-group" data-group="accessory">
+            <h4>Accessory</h4>
+            <button class="arrow left">&lt;</button>
+            <span class="letter">none</span>
+            <button class="arrow right">&gt;</button>
+          </div>
 
         </div>
+
         <div class="popup-right">
           <img src="assets/images/mumu.png" alt="decorative">
         </div>
+
       </div>
     </div>
   `;
@@ -112,16 +136,53 @@ function showCustomizePopup() {
     overlay.querySelector('button.close').onclick = () => overlay.remove();
     overlay.onclick = e => { if (e.target === overlay) overlay.remove(); };
 
+    // initialize colour pickers
     overlay.querySelectorAll('.color-group').forEach(groupEl => {
         const swatches = groupEl.querySelectorAll('.colour-option');
         swatches.forEach((swatch, i) => {
+
             swatch.style.backgroundColor = swatch.dataset.colour;
+
+            // click
             swatch.onclick = () => {
                 swatches.forEach(s => s.classList.remove('selected'));
                 swatch.classList.add('selected');
                 console.log(`picked for ${groupEl.dataset.group}:`, swatch.dataset.colour);
             };
+
             if (i === 0) swatch.classList.add('selected');
         });
     });
+
+
+    // initialize letter‐pickers
+    overlay.querySelectorAll('.letter-group').forEach(groupEl => {
+        const left  = groupEl.querySelector('.arrow.left');
+        const right = groupEl.querySelector('.arrow.right');
+        const span  = groupEl.querySelector('.letter');
+
+        groupEl.dataset.index = 0;
+        const update = () => {
+            const idx = parseInt(groupEl.dataset.index,10);
+            span.textContent = letters[idx];
+            console.log(`letter for ${groupEl.dataset.group}:`, letters[idx]);
+        };
+        left.onclick = () => {
+            let idx = parseInt(groupEl.dataset.index,10) - 1;
+            if (idx < 0) idx = letters.length - 1;
+            groupEl.dataset.index = idx;
+            update();
+        };
+        right.onclick = () => {
+            let idx = parseInt(groupEl.dataset.index,10) + 1;
+            if (idx >= letters.length) idx = 0;
+            groupEl.dataset.index = idx;
+            update();
+        };
+        update();
+    });
+
+
+
+
 }
