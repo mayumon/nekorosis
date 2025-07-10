d18b3706e8e32f0bff675f779d6eca535817f5dc
mayumon <malu.brisighello@mail.utoronto.ca>
2025-07-10 09:13:08 -0400

    added colour swatches to main, accent 1, and accent 2 settings for customization popup

--- js/header.js
+++ js/header.js
@@ -47,8 +47,6 @@ fetch('header.html')
 // CUSTOMIZE POPUP
 
 function showCustomizePopup() {
-
-    // create overlay
     const overlay = document.createElement('div');
     overlay.className = 'popup-overlay';
 
@@ -57,11 +55,49 @@ function showCustomizePopup() {
       <button class="close">&times;</button>
       <div class="popup-content">
         <div class="popup-left">
-          <h4>main</h4>
-          <h4>accent 1</h4>
-          <h4>accent 2</h4>
-          <h4>emotion</h4>
-          <h4>accessory</h4>
+
+          <div class="color-group" data-group="main">
+            <h4>Main</h4>
+            <div class="colour-options">
+              <div class="colour-option" data-colour="#e7dee3"></div>
+              <div class="colour-option" data-colour="#70686c"></div>
+              <div class="colour-option" data-colour="#373335"></div>
+              <div class="colour-option" data-colour="#f1d9bb"></div>
+              <div class="colour-option" data-colour="#e7a457"></div>
+              <div class="colour-option" data-colour="#5e3e35"></div>
+              <div class="colour-option" data-colour="#bce04c"></div>
+            </div>
+          </div>
+
+          <div class="color-group" data-group="accent1">
+            <h4>Accent 1</h4>
+            <div class="colour-options">
+              <div class="colour-option" data-colour="#e7dee3"></div>
+              <div class="colour-option" data-colour="#70686c"></div>
+              <div class="colour-option" data-colour="#373335"></div>
+              <div class="colour-option" data-colour="#f1d9bb"></div>
+              <div class="colour-option" data-colour="#e7a457"></div>
+              <div class="colour-option" data-colour="#5e3e35"></div>
+              <div class="colour-option" data-colour="#bce04c"></div>
+            </div>
+          </div>
+
+          <div class="color-group" data-group="accent2">
+            <h4>Accent 2</h4>
+            <div class="colour-options">
+              <div class="colour-option" data-colour="#e7dee3"></div>
+              <div class="colour-option" data-colour="#70686c"></div>
+              <div class="colour-option" data-colour="#373335"></div>
+              <div class="colour-option" data-colour="#f1d9bb"></div>
+              <div class="colour-option" data-colour="#e7a457"></div>
+              <div class="colour-option" data-colour="#5e3e35"></div>
+              <div class="colour-option" data-colour="#bce04c"></div>
+            </div>
+          </div>
+
+          <h4>Emotion</h4>
+          <h4>Accessory</h4>
+
         </div>
         <div class="popup-right">
           <img src="assets/images/mumu.png" alt="decorative">
@@ -72,10 +108,20 @@ function showCustomizePopup() {
 
     document.body.appendChild(overlay);
 
-    // wire up close button
-    overlay.querySelector('button.close').addEventListener('click', () => overlay.remove());
-    // also close if you click outside the box
-    overlay.addEventListener('click', e => {
-        if (e.target === overlay) overlay.remove();
+    // close handlers
+    overlay.querySelector('button.close').onclick = () => overlay.remove();
+    overlay.onclick = e => { if (e.target === overlay) overlay.remove(); };
+
+    overlay.querySelectorAll('.color-group').forEach(groupEl => {
+        const swatches = groupEl.querySelectorAll('.colour-option');
+        swatches.forEach((swatch, i) => {
+            swatch.style.backgroundColor = swatch.dataset.colour;
+            swatch.onclick = () => {
+                swatches.forEach(s => s.classList.remove('selected'));
+                swatch.classList.add('selected');
+                console.log(`picked for ${groupEl.dataset.group}:`, swatch.dataset.colour);
+            };
+            if (i === 0) swatch.classList.add('selected');
+        });
     });
 }
